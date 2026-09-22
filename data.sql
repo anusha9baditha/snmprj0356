279a41bc5df20cf8e5ee5d98d66abd5b149e765f
anusha9baditha <anusha@codegnan.com>
2026-09-22 10:19:17 +0530

    OTP verify

---
+++ data.sql
@@ -0,0 +1,138 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: snmprojectdb
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `emp_id` int NOT NULL,
+  `emp_name` varchar(25) DEFAULT NULL,
+  `dept_id` int DEFAULT NULL,
+  PRIMARY KEY (`emp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'a',101),(2,'B',101),(3,'C',102),(4,NULL,102);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `filesdata`
+--
+
+DROP TABLE IF EXISTS `filesdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `filesdata` (
+  `fileid` int unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(25) NOT NULL,
+  `filedata` longblob,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `userid` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`fileid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `filesdata_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `userdata` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filesdata`
+--
+
+LOCK TABLES `filesdata` WRITE;
+/*!40000 ALTER TABLE `filesdata` DISABLE KEYS */;
+/*!40000 ALTER TABLE `filesdata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notesdata`
+--
+
+DROP TABLE IF EXISTS `notesdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notesdata` (
+  `notesid` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `Content` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `userid` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`notesid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `notesdata_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `userdata` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notesdata`
+--
+
+LOCK TABLES `notesdata` WRITE;
+/*!40000 ALTER TABLE `notesdata` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notesdata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userdata`
+--
+
+DROP TABLE IF EXISTS `userdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `userdata` (
+  `userid` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `useremail` varchar(50) NOT NULL,
+  `userpassword` varchar(255) DEFAULT NULL,
+  `otp` char(6) DEFAULT NULL,
+  `otp_expiry_time` datetime DEFAULT NULL,
+  `account_status` enum('active','inactive','suspended') DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `useremail` (`useremail`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userdata`
+--
+
+LOCK TABLES `userdata` WRITE;
+/*!40000 ALTER TABLE `userdata` DISABLE KEYS */;
+INSERT INTO `userdata` VALUES (1,'anusha','anusha@codegnan.com','123',NULL,NULL,'active'),(6,'karthik','krishnakarthik599@gmail.com','123','U9lY1y','2026-09-19 10:57:02','inactive'),(7,'lakshmi','prasannavemula937@gmail.com','123','Z9xC5b','2026-09-21 10:17:47','inactive');
+/*!40000 ALTER TABLE `userdata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-22 10:19:00
